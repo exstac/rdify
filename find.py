b5c56f56cb91b5264d4b0e0e89bb14080c4c4f00
Oscar Vestlie <oscar@spotify.com>
2014-08-11 18:15:58 -0700

    Made everything a little clearer

--- find.py
+++ find.py
@@ -8,26 +8,32 @@ from google.appengine.ext import ndb
 
 import webapp2
 
-MAIN_PAGE_FOOTER_TEMPLATE = """\
+MAIN_PAGE_HEADER_TEMPLATE = """\
 <html><body>
     <form action="/" method="post">
-      <div><textarea name="content" rows="1" cols="30"></textarea></div>
-      <div><input type="submit" value="Search"></div>
+      <div><input style="width:200pt" type=text name="content"></input> <input type="submit" value="Search"></div>
     </form>
+"""
+MAIN_PAGE_FOOTER_TEMPLATE = """\
   </body>
 </html>
 """
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        self.response.write(MAIN_PAGE_HEADER_TEMPLATE)
         self.response.write(MAIN_PAGE_FOOTER_TEMPLATE)
 
     def post(self):
-        self.response.write('<html><body>')
+        self.response.write(MAIN_PAGE_HEADER_TEMPLATE)
         uri = self.request.get('content')
-        for result in self.fetch(uri):
-            self.response.write('<p>%s</p>' % (result,))
-        self.response.write('</html></body>')
+        results = self.fetch(uri)
+        self.response.write('<p>Found %d tracks:</p>' % (len(results),))
+        self.response.write('<ul>')
+        for result in results:
+            self.response.write('<li>%s</li>' % (result,))
+        self.response.write('</ul>')
+        self.response.write(MAIN_PAGE_FOOTER_TEMPLATE)
 
     def fetch(self, url):
         client = oauth.Client(oauth.Consumer('fxmhxjk9g4dhvsnmb8m376yy', 'azNqD6Cft7'))
@@ -47,13 +53,12 @@ class MainPage(webapp2.RequestHandler):
         result = []
         response = json.loads(urllib.urlopen(baseurl + query).read())
         for item in response['tracks']['items']:
-            if 'SE' not in item['album']['available_markets']:
-                continue
             open_link = item['external_urls']['spotify']
             artists = ', '.join([artist['name'] for artist in item['artists']])
             track = item['name']
             album = item['album']['name']
-            result.append('<a href="%s">%s - %s [%s]</a>' % (open_link, artists, track, album))
+            swe_cat = 'available' if 'SE' in item['album']['available_markets'] else 'unavailable'
+            result.append('<a href="%s">%s - %s [%s] (%s)</a>' % (open_link, artists, track, album, swe_cat))
         return result
 
 
